eaeab2373f015a127c9013822b96257c8ed33d12
robert2030 <robert7ashioya@gmail.com>
2024-11-08 12:52:59 +0300

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # File: 3-app.py
-# Author: Oluwatobiloba Light
 """Parameterize templates"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
